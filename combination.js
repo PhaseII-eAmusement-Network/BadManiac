d714c119cfa4151a9df3eb6e9294cd0a673fe746
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-02-28 11:42:40 -0800

    sex

--- combination.js
+++ combination.js
@@ -1,5 +1,6 @@
+const https = require('https')
 var express = require('express');
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 var app = express();
 var fs = require("fs");
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]})
@@ -16,6 +17,49 @@ client.on("messageCreate", msg => {
 
   if (msg.content === "ping") {
     msg.reply("pong");
+  }
+
+  if (msg.content.toLowerCase().includes('akeno')) {
+    msg.reply("we stan Rias in this household thank you very much. \n" +
+    "https://i.imgur.com/VpSmGlr.gif")
+  } else if (msg.content.toLowerCase().includes('rias')) {
+    msg.reply("thank you for following the rules. \n"+
+    "https://thumbs.gfycat.com/ColossalCreamyArrowworm-max-1mb.gif")
+  }
+
+  // Actual bot commands now lol
+  if (msg.content == 'BM! news') {
+    const options = new URL('https://restfulsleep.phaseii.network/getlatestnews');
+    
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+    
+        res.on('data', d => {
+            process.stdout.write(d)
+        })
+    })
+    
+    req.on('error', error => {
+        console.error(error)
+    })
+    
+    req.end()
+
+    msg.reply("")
+  } else if (msg.content == 'BM! help') {
+    msg.reply(
+        "```"+
+        "BadManiac is the bot designed for the PhaseII eAmusement Network. \n"+
+        "Used for DMing people their scorecards and other dumb shit. \n"+
+        "\n"+
+        "BadManiac commands: \n"+
+        "BM! help: Returns this help message. \n"+
+        "BM! news: Returns the latest news post. \n"+
+        "BM!: Sends YouTube link to Bad Maniacs. \n"+
+        "```"
+    )
+  } else if (msg.content == 'BM!') {
+    msg.reply("https://youtu.be/0WqzvPS_bDg")
   }
 })
 
