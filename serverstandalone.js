cd090222cd63692d62f1205ee0a29af078ce6b5f
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-02-27 16:40:53 -0800

    updates

--- serverstandalone.js
+++ serverstandalone.js
@@ -4,7 +4,6 @@ var fs = require("fs");
 
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( data );
       res.end( data );
    });
 })
